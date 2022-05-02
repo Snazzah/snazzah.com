1b0a09dfb8651ba3ab84c1a0c1de3d70981b8875
Snazzah <suggesttosnazzy@gmail.com>
2022-05-01 19:59:11 -0500

    Fix service worker

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,6 @@
 import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
 
 setupRouting();
-setupPrecaching(getFiles());
+setupPrecaching(getFiles().map((file) => {
+  if (file.url.startsWith('/assets/')) file.url = file.url.slice(8);
+}));
